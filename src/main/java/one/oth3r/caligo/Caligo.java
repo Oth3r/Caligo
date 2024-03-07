802fa33f6f08cc3cdd6b4118fdf5ebdd9db74347
Oth3r <oth3rone@outlook.com>
2024-03-07 16:20:01 -0600

    Initial commit

---
+++ src/main/java/one/oth3r/caligo/Caligo.java
@@ -0,0 +1,10 @@
+package one.oth3r.caligo;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Caligo implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/one/oth3r/caligo/client/CaligoClient.java
@@ -0,0 +1,10 @@
+package one.oth3r.caligo.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CaligoClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
