0915af9f29a25953206eeabfb7592b400aca6df3
Oth3r <oth3rone@outlook.com>
2024-04-24 14:15:38 -0500

    simplified Petrified effect logic

--- src/main/java/one/oth3r/caligo/effect/PetrifiedEffect.java
+++ src/main/java/one/oth3r/caligo/effect/PetrifiedEffect.java
@@ -3,22 +3,16 @@ package one.oth3r.caligo.effect;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
-import net.minecraft.entity.effect.StatusEffectInstance;
-import one.oth3r.caligo.Caligo;
 import one.oth3r.caligo.damage.ModDamageTypes;
 
 public class PetrifiedEffect extends StatusEffect {
-    protected PetrifiedEffect(StatusEffectCategory category, int color) {
-        super(category, color);
+    protected PetrifiedEffect() {
+        super(StatusEffectCategory.HARMFUL, 0x8f8f8f);
     }
     @Override
-    public void applyUpdateEffect(LivingEntity entity, int amplifier) {
-        super.applyUpdateEffect(entity, amplifier);
-        if (!entity.getWorld().isClient) {
-            StatusEffectInstance instance = entity.getStatusEffect(ModEffects.PETRIFIED);
-            if (instance == null) return;
-            entity.damage(ModDamageTypes.of(entity,ModDamageTypes.PETRIFIED), 1);
-        }
+    public boolean applyUpdateEffect(LivingEntity entity, int amplifier) {
+        entity.damage(entity.getDamageSources().create(ModDamageTypes.PETRIFIED), 1.0F);
+        return true;
     }
     @Override
     public boolean canApplyUpdateEffect(int duration, int amplifier) {
