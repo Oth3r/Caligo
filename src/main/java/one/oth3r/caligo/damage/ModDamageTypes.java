f738b14acbe7f5d3ea264ff5b6430e5b70f0500c
Oth3r <oth3rone@outlook.com>
2024-06-12 18:53:35 -0500

    petrified damage source with FabricASM

--- src/main/java/one/oth3r/caligo/damage/ModDamageTypes.java
+++ src/main/java/one/oth3r/caligo/damage/ModDamageTypes.java
@@ -10,6 +10,7 @@ import one.oth3r.caligo.Caligo;
 
 public class ModDamageTypes {
     public static final RegistryKey<DamageType> PETRIFIED = RegistryKey.of(RegistryKeys.DAMAGE_TYPE, new Identifier(Caligo.MOD_ID, "petrified_damage_type"));
+
     public static DamageSource of(Entity receiver, RegistryKey<DamageType> key) {
         return new DamageSource(receiver.getWorld().getRegistryManager().get(RegistryKeys.DAMAGE_TYPE).entryOf(key));
     }
