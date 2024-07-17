b4cb2016ab2f4b9b12fabf215e3d83b4f9595b14
Oth3r <oth3rone@outlook.com>
2024-07-17 11:04:38 -0500

    1.21 new block settings

--- src/main/java/one/oth3r/caligo/block/ModBlocks.java
+++ src/main/java/one/oth3r/caligo/block/ModBlocks.java
@@ -1,11 +1,8 @@
 package one.oth3r.caligo.block;
 
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
-import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.MapColor;
+import net.fabricmc.fabric.api.registry.LandPathNodeTypesRegistry;
+import net.minecraft.block.*;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactories;
@@ -13,7 +10,7 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import one.oth3r.caligo.Caligo;
-import one.oth3r.caligo.block.deepslate_statue.DeepslateStatueBlock;
+import one.oth3r.caligo.block.statue.deepslate.DeepslateStatueBlock;
 import one.oth3r.caligo.block.lumin_crystal.LuminCrystalBlock;
 import one.oth3r.caligo.block.lumin_crystal.LuminCrystalBlockEntity;
 import one.oth3r.caligo.block.statue.StatueBlock;
@@ -27,30 +24,30 @@ public class ModBlocks {
     // STATUE
 
     public static final Block STATUE_BLOCK = registerBlock("statue",
-            new StatueBlock(FabricBlockSettings.create()
+            new StatueBlock(AbstractBlock.Settings.create()
                     .mapColor(MapColor.STONE_GRAY).requiresTool().strength(1.5f, 6.0f)));
 
     public static final Block DEEPSLATE_STATUE_BLOCK = registerBlock("deepslate_statue",
-            new DeepslateStatueBlock(FabricBlockSettings.create()
+            new DeepslateStatueBlock(AbstractBlock.Settings.create()
                     .mapColor(MapColor.DEEPSLATE_GRAY).requiresTool().strength(1.5f, 6.0f)));
 
     public static final BlockEntityType<StatueBlockEntity> STATUE_BLOCK_ENTITY = Registry.register(
             Registries.BLOCK_ENTITY_TYPE,
             Identifier.of(Caligo.MOD_ID,"statue_block_entity"),
-            FabricBlockEntityTypeBuilder.create(StatueBlockEntity::new,STATUE_BLOCK,DEEPSLATE_STATUE_BLOCK).build());
+            BlockEntityType.Builder.create(StatueBlockEntity::new,STATUE_BLOCK,DEEPSLATE_STATUE_BLOCK).build());
 
     // LUMIN CRYSTAL
 
     public static final ToIntFunction<BlockState> toInt = value -> value.get(LuminCrystalBlock.POWER);
 
     public static final Block LUMIN_CRYSTAL_BLOCK = registerBlock("lumin_crystal",
-            new LuminCrystalBlock(FabricBlockSettings.create()
+            new LuminCrystalBlock(AbstractBlock.Settings.create()
                     .mapColor(MapColor.STONE_GRAY).luminance(toInt).requiresTool().strength(1.5f, 6.0f)));
 
     public static final BlockEntityType<LuminCrystalBlockEntity> LUMIN_CRYSTAL_BLOCK_ENTITY = Registry.register(
             Registries.BLOCK_ENTITY_TYPE,
             Identifier.of(Caligo.MOD_ID,"lumin_crystal_block_entity"),
-            FabricBlockEntityTypeBuilder.create(LuminCrystalBlockEntity::new, LUMIN_CRYSTAL_BLOCK).build());
+            BlockEntityType.Builder.create(LuminCrystalBlockEntity::new, LUMIN_CRYSTAL_BLOCK).build());
 
     private static Block registerBlock(String name, Block block) {
         return Registry.register(Registries.BLOCK,Identifier.of(Caligo.MOD_ID,name),block);
