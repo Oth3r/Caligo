533666cf13ecca9ac8fb9db6eec45a70269f9a7a
Oth3r <oth3rone@outlook.com>
2024-08-10 16:13:57 -0500

    creative tab

--- src/main/java/one/oth3r/caligo/Caligo.java
+++ src/main/java/one/oth3r/caligo/Caligo.java
@@ -9,6 +9,7 @@ import one.oth3r.caligo.effect.ModEffects;
 import one.oth3r.caligo.entity.ModEntities;
 import one.oth3r.caligo.generation.ModEntityGeneration;
 import one.oth3r.caligo.item.ModItems;
+import one.oth3r.caligo.itemgroup.ModItemGroups;
 import one.oth3r.caligo.potion.ModPotions;
 import one.oth3r.caligo.sound.ModSounds;
 import org.apache.logging.log4j.LogManager;
@@ -29,6 +30,7 @@ public class Caligo implements ModInitializer {
         ModItems.register();
         ModPotions.register();
         ModEntityGeneration.addSpawns();
+        ModItemGroups.register();
 
         // load the FabricASM changes to crash the game if something is wrong (mod compatibility ect)
         DamageEffects pattern = ClassTinkerers.getEnum(DamageEffects.class, "PETRIFICATION");
---
+++ src/main/java/one/oth3r/caligo/itemgroup/ModItemGroups.java
@@ -0,0 +1,43 @@
+package one.oth3r.caligo.itemgroup;
+
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+import one.oth3r.caligo.Caligo;
+import one.oth3r.caligo.item.ModItems;
+
+public class ModItemGroups {
+    public static final RegistryKey<ItemGroup> CALIGO_GROUP_KEY = RegistryKey.of(Registries.ITEM_GROUP.getKey(), Identifier.of(Caligo.MOD_ID, "caligo_group"));
+    public static final ItemGroup CALIGO_GROUP = FabricItemGroup.builder()
+            .icon(() -> new ItemStack(ModItems.STATUE))
+            .displayName(Text.translatable("itemGroup.caligo"))
+            .build();
+
+    public static void register() {
+        Registry.register(Registries.ITEM_GROUP, CALIGO_GROUP_KEY, CALIGO_GROUP);
+        fill();
+    }
+
+    /**
+     * fills the item groups with items
+     */
+    private static void fill() {
+        ItemGroupEvents.modifyEntriesEvent(CALIGO_GROUP_KEY).register(itemGroup -> {
+            itemGroup.add(ModItems.STATUE);
+            itemGroup.add(ModItems.DEEPSLATE_STATUE);
+            itemGroup.add(ModItems.STROW_SPAWN_EGG);
+            itemGroup.add(ModItems.DEEP_STROW_SPAWN_EGG);
+            itemGroup.add(ModItems.STROW_ESSENCE);
+            itemGroup.add(ModItems.LUMIN_CRYSTAL);
+            itemGroup.add(ModItems.SMALL_ORE_REMAINS);
+            itemGroup.add(ModItems.ORE_REMAINS);
+            itemGroup.add(ModItems.LARGE_ORE_REMAINS);
+        });
+    }
+}
