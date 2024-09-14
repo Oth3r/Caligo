a09d9e95b7b31bee2cdf45d70bda19b410e573ed
Oth3r <oth3rone@outlook.com>
2024-09-13 21:12:16 -0500

    coppice spawn egg

--- src/main/java/one/oth3r/caligo/datagen/custom/CoppiceProviders.java
+++ src/main/java/one/oth3r/caligo/datagen/custom/CoppiceProviders.java
@@ -1,10 +1,13 @@
 package one.oth3r.caligo.datagen.custom;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.fabricmc.fabric.api.datagen.v1.provider.SimpleFabricLootTableProvider;
 import net.fabricmc.fabric.api.tag.convention.v2.ConventionalItemTags;
 import net.minecraft.block.Blocks;
+import net.minecraft.data.client.BlockStateModelGenerator;
+import net.minecraft.data.client.ItemModelGenerator;
 import net.minecraft.item.Items;
 import net.minecraft.loot.LootPool;
 import net.minecraft.loot.LootTable;
@@ -14,6 +17,7 @@ import net.minecraft.loot.provider.number.UniformLootNumberProvider;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.registry.tag.ItemTags;
+import one.oth3r.caligo.datagen.ModModelProvider;
 import one.oth3r.caligo.item.ModItems;
 import one.oth3r.caligo.loot_table.ModLootTables;
 import one.oth3r.caligo.tag.ModBlockTags;
@@ -84,6 +88,27 @@ public class CoppiceProviders {
                             .with(ItemEntry.builder(ModItems.ORE_REMAINS).weight(2))
                             .with(ItemEntry.builder(ModItems.LARGE_ORE_REMAINS)))
                     .randomSequenceId(ModLootTables.COPPICE_GEM_REMAINS.getRegistry()));
+        }
+
+        @Override
+        public String getName() {
+            return "Coppice "+super.getName();
+        }
+    }
+
+    public static class Model extends FabricModelProvider {
+
+        public Model(FabricDataOutput output) {
+            super(output);
+        }
+
+        @Override
+        public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {}
+
+        @Override
+        public void generateItemModels(ItemModelGenerator itemModelGenerator) {
+            // SPAWN EGGS
+            itemModelGenerator.register(ModItems.COPPICE_SPAWN_EGG, ModModelProvider.SPAWN_EGG);
         }
 
         @Override
--- src/main/java/one/oth3r/caligo/item/ModItems.java
+++ src/main/java/one/oth3r/caligo/item/ModItems.java
@@ -1,6 +1,5 @@
 package one.oth3r.caligo.item;
 
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -10,6 +9,16 @@ import one.oth3r.caligo.block.ModBlocks;
 import one.oth3r.caligo.entity.ModEntities;
 
 public class ModItems {
+    public static final Item STROW_SPAWN_EGG = registerItem("strow_spawn_egg",
+            new SpawnEggItem(ModEntities.STROW, 0x808080, 0xa0a0a0, new Item.Settings()));
+
+    public static final Item DEEP_STROW_SPAWN_EGG = registerItem("deep_strow_spawn_egg",
+            new SpawnEggItem(ModEntities.DEEP_STROW, 0x33333b, 0x797979, new Item.Settings()));
+
+    public static final Item COPPICE_SPAWN_EGG = registerItem("coppice_spawn_egg",
+            new SpawnEggItem(ModEntities.COPPICE, 0x60772b,0xa0a0a0 , new Item.Settings()));
+
+
     public static final Item STROW_ESSENCE = registerItem("strow_essence",
             new Item(new Item.Settings()));
 
@@ -21,12 +30,6 @@ public class ModItems {
 
     public static final Item SMALL_ORE_REMAINS = registerItem("small_ore_remains",
             new Item(new Item.Settings()));
-
-    public static final Item STROW_SPAWN_EGG = registerItem("strow_spawn_egg",
-            new SpawnEggItem(ModEntities.STROW, 0x808080, 0xa0a0a0, new Item.Settings()));
-
-    public static final Item DEEP_STROW_SPAWN_EGG = registerItem("deep_strow_spawn_egg",
-            new SpawnEggItem(ModEntities.DEEP_STROW, 0x33333b, 0x797979, new Item.Settings()));
 
     public static final Item STATUE = registerItem("statue",
             new BlockItem(ModBlocks.STATUE_BLOCK, new Item.Settings()));
@@ -40,6 +43,9 @@ public class ModItems {
     public static final Item DRIPLEAF_VINES = registerItem("dripleaf_vines",
             new BlockItem(ModBlocks.DRIPLEAF_VINES, new Item.Settings()));
 
+    public static final Item PETUNIA = registerItem("petunia",
+            new BlockItem(ModBlocks.PETUNIA, new Item.Settings()));
+
     public static final Item LUSH_MARIGOLD = registerItem("lush_marigold",
             new PlaceableOnWaterItem(ModBlocks.LUSH_MARIGOLD, new Item.Settings()));
 
@@ -48,10 +54,6 @@ public class ModItems {
     }
 
     public static void register() {
-        // add the spawn eggs to the spawn eggs tab, make it alphabetical
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.SPAWN_EGGS).register(content -> {
-            content.addAfter(Items.STRIDER_SPAWN_EGG,STROW_SPAWN_EGG);
-            content.addAfter(Items.CREEPER_SPAWN_EGG,DEEP_STROW_SPAWN_EGG);
-        });
+
     }
 }
